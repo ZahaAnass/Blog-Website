d0e2399b8c4d7c674df19e53fdb1a11feae8a344
ZahaAnass <zahaanass277@gmail.com>
2025-06-28 22:02:35 +0100

    feat: update navbar with fixed positioning and dark mode support

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function NavBar(){
 
     return(
         <>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900 backdrop-blur-2xl">
+            <nav className="bg-white border-gray-200 dark:bg-gray-800 backdrop-blur-3xl dark:backdrop-blur-3xl fixed top-0 left-0 right-0 z-50">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <FontAwesomeIcon icon={faBook} color="white" />
@@ -22,7 +22,7 @@ function NavBar(){
                         <FontAwesomeIcon icon={faBars} />
                     </Button>
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
                             <li>
                                 <Link to="/" className="block py-2 px-3 border-b-2 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
                             </li>
