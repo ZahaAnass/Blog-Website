530624df1259d3e6ce1513f5ca1700351422acb2
ZahaAnass <zahaanass277@gmail.com>
2025-07-01 15:31:55 +0100

    Update App.jsx with new routing and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import NavBar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
-// import Contact from './pages/Contact'
+import NotFound from './pages/NotFount'
+import Contact from './pages/Contact'
 // import Blog from './pages/Blog'
 // import BlogDetails from './pages/Blog-details'
 function App() {
@@ -17,9 +18,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact /> } /> */}
+          <Route path="/contact" element={<Contact /> } />
           {/* <Route path="/blog" element={<Blog />} /> */}
           {/*<Route path="/blog/:id" element={<BlogDetails />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
