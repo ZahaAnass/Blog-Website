9977f574c3924fcc9afaef9d9986726b7d212f19
ZahaAnass <zahaanass277@gmail.com>
2025-07-01 15:31:55 +0100

    Update Navbar component with responsive design and active link highlighting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars, faBook } from "@fortawesome/free-solid-svg-icons"
 import { Button } from "flowbite-react";
+import { useLocation } from "react-router-dom";
 
 function NavBar(){
+
+    const location = useLocation();
 
     const ToggleNav = () => {
         document.getElementById('navbar-default').classList.toggle('hidden');
@@ -24,16 +27,56 @@ function NavBar(){
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
                             <li>
-                                <Link onClick={ToggleNav} to="/" className="block py-2 px-3 border-b-2 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                            <Link 
+                                onClick={ToggleNav} 
+                                to="/" 
+                                className={`block py-2 px-3 border-b-2 rounded-sm md:border-0 md:p-0 ${
+                                    location.pathname === "/" 
+                                    ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500" 
+                                    : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                }`}
+                                >
+                                Home
+                            </Link>
                             </li>
                             <li>
-                                <Link onClick={ToggleNav} to="/about" className="block py-2 px-3 border-b-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
+                                <Link 
+                                    onClick={ToggleNav} 
+                                    to="/about" 
+                                    className={`block py-2 px-3 border-b-2 rounded-sm md:border-0 md:p-0 ${
+                                        location.pathname === "/about" 
+                                        ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500" 
+                                        : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    }`}
+                                >
+                                    About
+                                </Link>
                             </li>
                             <li>
-                                <Link onClick={ToggleNav} to="/blog" className="block py-2 px-3 border-b-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</Link>
+                                <Link 
+                                    onClick={ToggleNav} 
+                                    to="/blog" 
+                                    className={`block py-2 px-3 border-b-2 rounded-sm md:border-0 md:p-0 ${
+                                        location.pathname === "/blog" 
+                                        ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500" 
+                                        : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    }`}
+                                >
+                                    Blog
+                                </Link>
                             </li>
                             <li>
-                                <Link onClick={ToggleNav} to="/contact" className="block py-2 px-3 border-b-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+                                <Link 
+                                    onClick={ToggleNav} 
+                                    to="/contact" 
+                                    className={`block py-2 px-3 border-b-2 rounded-sm md:border-0 md:p-0 ${
+                                        location.pathname === "/contact" 
+                                        ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500" 
+                                        : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    }`}
+                                >
+                                    Contact
+                                </Link>
                             </li>
                         </ul>
                     </div>
