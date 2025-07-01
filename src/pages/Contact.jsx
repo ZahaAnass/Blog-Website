c0f7a3070409c0083ae79cd6b7d7de4766c8b809
ZahaAnass <zahaanass277@gmail.com>
2025-07-01 15:31:55 +0100

    Update Contact page with new layout and components

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,24 @@
+import Hero from "../components/Contact/Hero";
+import ContactForm from "../components/Contact/ContactForm";
+
+export default function Contact() {
+    
+
+
+    return(
+        <>
+            <div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-100 ">
+                <div className=" mx-auto px-4 py-22 mt-16 dark:bg-gray-900">
+                    <div className="max-w-6xl mx-auto">
+                        {/* Hero */}
+                        <Hero />
+
+                        {/* Contact Form */}
+                        <ContactForm />
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
