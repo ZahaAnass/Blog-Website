c6d7d363b3a87fac9f010739643d80f8b0cb0fab
ZahaAnass <zahaanass277@gmail.com>
2025-07-01 20:47:49 +0100

    feat: update App.jsx with new routing and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import NavBar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFount'
 import Contact from './pages/Contact'
-// import Blog from './pages/Blog'
+import Blog from './pages/Blog'
 // import BlogDetails from './pages/Blog-details'
 function App() {
 
@@ -19,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact /> } />
-          {/* <Route path="/blog" element={<Blog />} /> */}
+          <Route path="/blog" element={<Blog />} />
           {/*<Route path="/blog/:id" element={<BlogDetails />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
