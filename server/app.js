a2ceab5fcf9a4a5707446a41b5e03d3af668e154
ZahaAnass <zahaanass277@gmail.com>
2025-07-20 00:02:38 +0100

    update main application configuration and middleware setup

---
+++ server/app.js
@@ -0,0 +1,59 @@
+require("dotenv").config()
+require("express-async-errors")
+const express = require("express")
+const app = express()
+
+// extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+// connectDB
+const connectDB = require("./db/connect")
+const authenticateUser = require("./middleware/authentification")
+
+// routers
+const authRoutes = require("./routes/auth")
+const blogRoutes = require("./routes/blog")
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
+app.use(express.json())
+
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.get("/", (req, res) => {
+    res.send("<h1>Blog Api</h1>")
+})
+
+// routes
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/blog", authenticateUser, blogRoutes)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    }catch(error){
+        console.log(error)
+    }
+};
+
+start();
