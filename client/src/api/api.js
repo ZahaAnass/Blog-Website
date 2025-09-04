ea1858592dc16d4f95e371df3c41598661d94605
ZahaAnass <zahaanass277@gmail.com>
2025-09-04 21:27:06 +0100

    improve the apis

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -15,12 +15,12 @@ API.interceptors.request.use((config) => {
 // User 
 export const UserSignUp = async (user) => {
     const response = await API.post("/auth/register", user)
-    return response.data
+    return response
 }
 
 export const UserLogin = async (user) => {
     const response = await API.post("/auth/login", user)
-    return response.data
+    return response
 }
 
 // Blog
@@ -28,33 +28,33 @@ export const getAllBlogs = async (token) => {
     const response = await API.get("/blog", {
         headers: { Authorization: `Bearer ${token}` }
     })
-    return response.data
+    return response
 }
 
 export const getBlogById = async (id, token) => {
     const response = await API.get(`/blog/${id}`, {
         headers: { Authorization: `Bearer ${token}` }
     })
-    return response.data
+    return response
 }
 
 export const updateBlog = async (id, blog, token) => {
     const response = await API.put(`/blog/${id}`, blog, {
         headers: { Authorization: `Bearer ${token}` }
     })
-    return response.data
+    return response
 }
 
 export const deleteBlog = async (id, token) => {
     const response = await API.delete(`/blog/${id}`, {
         headers: { Authorization: `Bearer ${token}` }
     })
-    return response.data
+    return response
 }
 
 export const createBlog = async (blog, token) => {
     const response = await API.post("/blog", blog, {
         headers: { Authorization: `Bearer ${token}` }
     })
-    return response.data
+    return response
 }
