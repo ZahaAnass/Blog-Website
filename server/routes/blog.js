0d7a870f564b0c5df1fa6ea529b54594698b493d
ZahaAnass <zahaanass277@gmail.com>
2025-09-07 17:29:42 +0100

    add getBlogCategories route

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { createBlog, getAllBlogs, getBlog, updateBlog, deleteBlog } = require("../controllers/blog")
+const { createBlog, getAllBlogs, getBlog, updateBlog, deleteBlog, getBlogCategories } = require("../controllers/blog")
 
 router.route("/").get(getAllBlogs).post(createBlog)
+router.route("/categories").get(getBlogCategories)
 router.route("/:id").get(getBlog).patch(updateBlog).delete(deleteBlog)
 
 module.exports = router
