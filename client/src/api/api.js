7222792c05cb2628aefeb3de046fbb9af4d73ceb
ZahaAnass <zahaanass277@gmail.com>
2025-09-09 10:19:29 +0100

    update api to use token directly via api interceptors

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -24,44 +24,39 @@ export const UserLogin = async (user) => {
 }
 
 // Blog
-export const getAllBlogs = async (token) => {
-    const response = await API.get("/blogs", {
-        headers: { Authorization: `Bearer ${token}` }
-    })
+export const getAllBlogs = async () => {
+    const response = await API.get("/blogs")
     return response
 }
 
-export const getBlogById = async (id, token) => {
-    const response = await API.get(`/blogs/${id}`, {
-        headers: { Authorization: `Bearer ${token}` }
-    })
+export const getBlogById = async (id) => {
+    const response = await API.get(`/blogs/${id}`)
     return response
 }
 
-export const getBlogCategories = async (token) => {
-    const response = await API.get("/blogs/categories", {
-        headers: { Authorization: `Bearer ${token}` }
-    })
+export const getBlogCategories = async () => {
+    const response = await API.get("/blogs/categories")
     return response
 }
 
-export const updateBlog = async (id, blog, token) => {
-    const response = await API.put(`/blog/${id}`, blog, {
-        headers: { Authorization: `Bearer ${token}` }
-    })
+// export const updateBlog = async (id, blog, token) => {
+//     const response = await API.patch(`/blogs/${id}`, blog, {
+//         headers: { Authorization: `Bearer ${token}` }
+//     })
+//     return response
+// }
+
+export const updateBlog = async (id, blog) => {
+    const response = await API.patch(`/blogs/${id}`, blog)
     return response
 }
 
-export const deleteBlog = async (id, token) => {
-    const response = await API.delete(`/blogs/${id}`, {
-        headers: { Authorization: `Bearer ${token}` }
-    })
+export const deleteBlog = async (id) => {
+    const response = await API.delete(`/blogs/${id}`)
     return response
 }
 
-export const createBlog = async (blog, token) => {
-    const response = await API.post("/blog", blog, {
-        headers: { Authorization: `Bearer ${token}` }
-    })
+export const createBlog = async (blog) => {
+    const response = await API.post("/blogs", blog)
     return response
 }
